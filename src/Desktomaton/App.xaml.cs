b6ac2f69d57868ac638ae936b958af39abf267ee
Oren Nachman <nachmano@amazon.com>
2020-08-18 00:09:52 +1000

    Move to Prism for MVVM

--- src/Desktomaton/App.xaml.cs
+++ src/Desktomaton/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Desktomaton.Views;
+using Prism.Ioc;
+using Prism.Unity;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -11,7 +14,18 @@ namespace Desktomaton
   /// <summary>
   /// Interaction logic for App.xaml
   /// </summary>
-  public partial class App : Application
+  public partial class App : PrismApplication
   {
+    protected override Window CreateShell()
+    {
+      var mainWindow = Container.Resolve<MainWindow>();
+
+      return mainWindow;
+    }
+
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+      
+    }
   }
 }
