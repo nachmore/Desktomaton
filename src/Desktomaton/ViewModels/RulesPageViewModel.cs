85b94d4e7b24f374356b2f6af55d731811a30b21
Oren Nachman <code@nachmore.com>
2020-08-18 01:12:36 +1000

    Use ModernWpf for presentation, with Prism wiring

---
+++ src/Desktomaton/ViewModels/RulesPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desktomaton.ViewModels
+{
+  class RulesPageViewModel : BindableBase
+  {
+
+    public RulesPageViewModel()
+    {
+
+    }
+
+    public string HelloThere => "Hello There!";
+  }
+}
---
+++ src/Desktomaton/Views/ExecutionLogsPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Desktomaton.Views
+{
+  /// <summary>
+  /// Interaction logic for ExecutionLogs.xaml
+  /// </summary>
+  public partial class ExecutionLogsPage
+  {
+    public ExecutionLogsPage()
+    {
+      InitializeComponent();
+    }
+  }
+}
