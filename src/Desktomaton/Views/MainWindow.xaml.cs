acf04202d245e067f4b8f871f2053611c041e07f
Oren Nachman <code@nachmore.com>
2020-08-18 00:09:52 +1000

    Move to Prism for MVVM

---
+++ src/Desktomaton/Views/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Desktomaton.PluginBase;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Desktomaton.Views
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    private RulesManagement.RuleGroup _ruleGroup = new RulesManagement.RuleGroup();
+
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+  }
+}
