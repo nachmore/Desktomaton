e82cc9c21b476a0b701303f7c2e24a3b895d1b3e
Oren Nachman <nachmano@amazon.com>
2020-09-23 23:01:41 +1000

    NotificationPlugin: New action that sends notifications
    Note: Does not use the native Windows library which is somewhat
    restrictive right now, though may transition to that in the future.

---
+++ src/Plugins/Desktomaton.Plugins.Notification/DesktomatonNotification/NotificationData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using ToastNotifications.Core;
+
+namespace Desktomaton.Plugins.Notification.DesktomatonNotification
+{
+  // Not using INotifyPropertyChanged since this class is recreated for each notification
+  internal class NotificationData : NotificationBase
+  {
+    private CustomDisplayPart _displayPart;
+    public override NotificationDisplayPart DisplayPart => _displayPart ??= new CustomDisplayPart(this);
+
+    public string Icon { get; set; }
+    public string Title { get; set; }
+
+    public NotificationData(string message) : this(message, new MessageOptions())
+    {
+    }
+
+    public NotificationData(string message, MessageOptions options) : base(message, options)
+    {
+    }
+  }
+}
