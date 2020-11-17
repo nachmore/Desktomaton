e4524d3250048765222715c6448a73eedf5ab87c
Oren Nachman <code@nachmore.com>
2020-11-18 07:59:05 +1100

    Start of UX

--- src/Desktomaton/ViewModels/RulesPageViewModel.cs
+++ src/Desktomaton/ViewModels/RulesPageViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Prism.Mvvm;
+﻿using Desktomaton.RulesManagement;
+using Prism.Commands;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,11 +10,25 @@ namespace Desktomaton.ViewModels
   class RulesPageViewModel : BindableBase
   {
 
+    public List<RuleGroup> RuleGroups { 
+      get
+      {
+        return null;
+      } 
+    }
+
     public RulesPageViewModel()
     {
 
     }
 
-    public string HelloThere => "Hello There!";
+    private DelegateCommand _commandCreateRuleGroup = null;
+    public DelegateCommand CommandCreateRuleGroup =>
+        _commandCreateRuleGroup ?? (_commandCreateRuleGroup = new DelegateCommand(CommandCreateRuleGroupExecute));
+
+    private void CommandCreateRuleGroupExecute()
+    {
+      System.Windows.MessageBox.Show("yo");
+    }
   }
 }
