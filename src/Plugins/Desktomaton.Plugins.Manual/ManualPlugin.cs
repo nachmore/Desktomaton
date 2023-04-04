0dfd5376ba5eb2cd8904d081b41fef18b78b6de7
Oren Nachman <code@nachmore.com>
2023-04-04 17:26:49 +0100

    Add support for a manual trigger
    Includes a button, though no code behind (that's in .private). In theory this would work by defining the values within the UX and being able to press a button to "trigger now".

---
+++ src/Plugins/Desktomaton.Plugins.Manual/ManualPlugin.cs
@@ -0,0 +1,21 @@
+﻿using Desktomaton.PluginBase;
+
+namespace Desktomaton.Plugins.Manual
+{
+  [Serializable]
+  public class ManualPlugin : DesktomatonTrigger
+  {
+    public override string Name => "Manual";
+
+    [DesktomatonProperty(PrettyTitle = "Current Value")]
+    public string? CurrentValue { get; set; }
+
+    [DesktomatonProperty(PrettyTitle = "Trigger Value")]
+    public string? TriggerValue { get; set; }
+
+    public override async Task<bool> EvaluateAsync()
+    {
+      return TriggerValue == CurrentValue;
+    }
+  }
+}
